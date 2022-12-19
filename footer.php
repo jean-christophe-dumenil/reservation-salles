447bf801e36effba614ce50d6386788694f23390
jean-christophe-dumenil <jean-christophe.dumenil@laplateforme.io>
2022-12-19 14:45:31 +0100

    ajout des image dans le footer pour les liens acceuil et github

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 	</div>
 	<div class="footer-left">
 		<p class="footer-links">
-			<a class="link-1" href="index.php">Accueil</a>
-   			<a href="inscription.php">Inscription</a>
-			<a href="connexion.php">Connexion</a>
+			<a class="link-1" href="index.php"><img src="images.png" alt="acceuil"/></a>
+   			<a class="link-1" href="https://github.com/jean-christophe-dumenil/reservation-salles"><img src="GitHub.png" alt="github" /></a>
+			
 		</p>
 		
 	</div>
